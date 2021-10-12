aa06db7fa7d376001e7a23fbffc7de7a1a418a25
Tim Kelly <tkelly3118@gmail.com>
2021-10-12 09:23:49 +0100

    Init

---
+++ JavaLca/Node.java
@@ -0,0 +1,16 @@
+package JavaLca;
+// Java Program for Lowest Common Ancestor in a Binary Tree
+// A O(n) solution to find LCA of two given values n1 and n2
+
+
+// A Binary Tree node
+class Node {
+	int data;
+	Node left, right;
+
+	Node(int value) {
+		data = value;
+		left = right = null;
+	}
+}
+
