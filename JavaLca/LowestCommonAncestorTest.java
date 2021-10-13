28189e88b11918d6e9ff31f0b22f367d36eaedae
Tim Kelly <tkelly3118@gmail.com>
2021-10-13 15:11:50 +0100

    First tests

--- JavaLca/LowestCommonAncestorTest.java
+++ JavaLca/LowestCommonAncestorTest.java
@@ -6,5 +6,30 @@ import org.junit.Test;
 
 public class LowestCommonAncestorTest {
 
+    @Test
+    public void testFindLCA() {
+        LowestCommonAncestor tree = new LowestCommonAncestor();
+		tree.root = new Node(1);
+		tree.root.left = new Node(2);
+		tree.root.right = new Node(3);
+		tree.root.left.left = new Node(4);
+		tree.root.left.right = new Node(5);
+		tree.root.right.left = new Node(6);
+		tree.root.right.right = new Node(7);
+
+         assertEquals(2, tree.findLCA(4,5));
+         assertEquals(1, tree.findLCA(4,6));
+    }
+
+    @Test
+    public void testFindLCAInternal() {
+
+    }
+
+    @Test
+    public void testFindPath() {
+
+    }
+
 	
 }
