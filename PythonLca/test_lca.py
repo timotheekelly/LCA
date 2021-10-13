dab7e31744beb892530244c3050b9ea3821588fa
Tim Kelly <tkelly3118@gmail.com>
2021-10-13 20:50:01 +0100

    Update test_lca.py

--- PythonLca/test_lca.py
+++ PythonLca/test_lca.py
@@ -5,6 +5,9 @@ class TestNode(unittest.TestCase):
 
         def test_node4_5(self):
             self.assertEqual(lca.findLCA(lca.root, 4, 5), 2)
+        
+        def test_node6_2(self):
+            self.assertEqual(lca.findLCA(lca.root, 6, 2), 1)
 
 if __name__ == '__main__':
     unittest.main()
