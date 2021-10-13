2d6936d6384c72eb64c30cb57c0b620098a41e7b
Tim Kelly <tkelly3118@gmail.com>
2021-10-13 21:02:53 +0100

    finished py

--- PythonLca/test_lca.py
+++ PythonLca/test_lca.py
@@ -3,14 +3,19 @@ import lca
 
 class TestNode(unittest.TestCase):
 
-        def test_node4_5(self):
+        def test_node_4_5(self):
             self.assertEqual(lca.findLCA(lca.root, 4, 5), 2)
         
-        def test_node6_2(self):
+        def test_node_6_2(self):
             self.assertEqual(lca.findLCA(lca.root, 6, 2), 1)
 
         def test_node_none(self):
-            self.assertEqual(lca.findLCA(None, 4, 5), -1)      
+            self.assertEqual(lca.findLCA(None, 4, 5), -1) 
 
-if __name__ == '__main__':
-    unittest.main()
+        def test_node_4_13(self):
+            self.assertEqual(lca.findLCA(lca.root, 4, 13), -1) 
+
+        def test_node_13_4(self):
+            self.assertEqual(lca.findLCA(lca.root, 13, 4), -1) 
+
+             
