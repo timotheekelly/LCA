1cb1051b4107aa43d96d8b55db818b12d13ab648
Tim Kelly <tkelly3118@gmail.com>
2021-10-13 12:42:54 +0100

    Adjustments

---
+++ JavaLca/LowestCommonAncestorTest.java
@@ -0,0 +1,10 @@
+package JavaLca;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class LowestCommonAncestorTest {
+
+	
+}
--- PythonLca/lca.py
+++ PythonLca/lca.py
@@ -1,46 +1,72 @@
-class TreeNode:
-   def __init__(self, data, left = None, right = None):
-      self.data = data
-      self.left = left
-      self.right = right
-def insert(temp,data):
-   que = []
-   que.append(temp)
-   while (len(que)):
-      temp = que[0]
-      que.pop(0)
-      if (not temp.left):
-         if data is not None:
-            temp.left = TreeNode(data)
-         else:
-            temp.left = TreeNode(0)
-         break
-      else:
-         que.append(temp.left)
-         if (not temp.right):
-            if data is not None:
-               temp.right = TreeNode(data)
-            else:
-               temp.right = TreeNode(0)
+# A binary tree node
+class Node:
+    # Constructor to create a new binary node
+    def __init__(self, key):
+        self.key =  key
+        self.left = None
+        self.right = None
+ 
+# Finds the path from root node to given root of the tree.
+# Stores the path in a list path[], returns true if path
+# exists otherwise false
+def findPath( root, path, k):
+ 
+    # Baes Case
+    if root is None:
+        return False
+ 
+    # Store this node is path vector. The node will be
+    # removed if not in path from root to k
+    path.append(root.key)
+ 
+    # See if the k is same as root's key
+    if root.key == k :
+        return True
+ 
+    # Check if k is found in left or right sub-tree
+    if ((root.left != None and findPath(root.left, path, k)) or
+            (root.right!= None and findPath(root.right, path, k))):
+        return True
+ 
+    # If not present in subtree rooted with root, remove
+    # root from path and return False
+      
+    path.pop()
+    return False
+ 
+# Returns LCA if node n1 , n2 are present in the given
+# binary tre otherwise return -1
+def findLCA(root, n1, n2):
+ 
+    # To store paths to n1 and n2 fromthe root
+    path1 = []
+    path2 = []
+ 
+    # Find paths from root to n1 and root to n2.
+    # If either n1 or n2 is not present , return -1
+    if (not findPath(root, path1, n1) or not findPath(root, path2, n2)):
+        return -1
+ 
+    # Compare the paths to get the first different value
+    i = 0
+    while(i < len(path1) and i < len(path2)):
+        if path1[i] != path2[i]:
             break
-         else:
-            que.append(temp.right)
-def make_tree(elements):
-   Tree = TreeNode(elements[0])
-   for element in elements[1:]:
-      insert(Tree, element)
-   return Tree
-class Solution(object):
-   def lowestCommonAncestor(self, root, p, q):
-      if not root:
-         return None
-      if root.data == p or root.data ==q:
-         return root
-      left = self.lowestCommonAncestor(root.left, p, q)
-      right = self.lowestCommonAncestor(root.right, p, q)
-      if right and left:
-         return root
-      return right or left
-ob1 = Solution()
-tree = make_tree([5,1,6,3,0,8,None,None,7,4])
-print(ob1.lowestCommonAncestor(tree, 3, 1).data)
+        i += 1
+    return path1[i-1]
+ 
+ 
+# Driver program to test above function
+# Let's create the Binary Tree shown in above diagram
+root = Node(1)
+root.left = Node(2)
+root.right = Node(3)
+root.left.left = Node(4)
+root.left.right = Node(5)
+root.right.left = Node(6)
+root.right.right = Node(7)
+ 
+print ("LCA(4, 5) = %d" %(findLCA(root, 4, 5,)))
+print ("LCA(4, 6) = %d" %(findLCA(root, 4, 6)))
+print ("LCA(3, 4) = %d" %(findLCA(root,3,4)))
+print ("LCA(2, 4) = %d" %(findLCA(root,2, 4)))
