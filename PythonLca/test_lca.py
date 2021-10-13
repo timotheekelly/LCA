885eaba0dd9a9bc8750964fcf0f66216e7cd90f4
Tim Kelly <tkelly3118@gmail.com>
2021-10-13 20:57:12 +0100

    checking null

--- PythonLca/test_lca.py
+++ PythonLca/test_lca.py
@@ -9,5 +9,8 @@ class TestNode(unittest.TestCase):
         def test_node6_2(self):
             self.assertEqual(lca.findLCA(lca.root, 6, 2), 1)
 
+        def test_node_none(self):
+            self.assertEqual(lca.findLCA(None, 4, 5), -1)      
+
 if __name__ == '__main__':
     unittest.main()
